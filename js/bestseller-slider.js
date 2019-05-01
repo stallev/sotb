2228165f26f149afa8c82f88e75a0818cf7490bc
alex <stwork87@gmail.com>
2019-05-02 00:49:15 +0300

    add 90% styles

---
+++ js/bestseller-slider.js
@@ -0,0 +1,28 @@
+'use string';
+var nextSlideButton = document.getElementsByClassName('bestsellers-slider__controls-next');
+var prevSlideButton = document.getElementsByClassName('bestsellers-slider__controls-prev');
+var slideIndex = 1;
+showSlides(slideIndex);
+nextSlideButton.addEventlistener('click', plusSlides);
+nextSlideButton.addEventlistener('click', plusSlides);
+function plusSlides(){
+  slideIndex += 1;
+}
+function minusSlides(){
+  slideIndex -= 1;
+}
+
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName('bestseller');
+  if (n > slides.length){
+    slideIndex = 1;
+  }
+  if (n < 1){
+    slideIndex = slides.length;
+  }
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
